8219fd8c70be6ec8cdc5cead7fbd37c79f4884d3
nicolas-martin <nicolas.martin.0986@gmail.com>
2021-05-20 17:58:31 -0400

    remove any edns manipulation

--- resolver/upstream_resolver.go
+++ resolver/upstream_resolver.go
@@ -102,27 +102,6 @@ func (r *httpUpstreamClient) callExternal(msg *dns.Msg,
 }
 
 func (r *dnsUpstreamClient) callExternal(msg *dns.Msg, upstreamURL string) (response *dns.Msg, rtt time.Duration, err error) {
-	opt := msg.IsEdns0()
-
-	if opt != nil {
-		if len(opt.Option) != 0 {
-			for i, v := range opt.Option {
-				switch v.(type) {
-				case *dns.EDNS0_LOCAL:
-					msg.Extra = append(msg.Extra[:i], msg.Extra[i+1:]...)
-				}
-			}
-
-		}
-
-	}
-
-	// opt.Option = nil
-	mac, _ := getMacFromEDNS0(msg)
-	if len(mac) != 0 {
-		logger("external call").Info("edns mac present in external call")
-	}
-
 	return r.client.Exchange(msg, upstreamURL)
 }
 
